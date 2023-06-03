7d246624d15f5b0ed6605dcb52863bb464d99e21
Richelle <richelle.ak@gmail.com>
2023-06-03 13:19:43 -0700

    update inventory control

---
+++ src/components/CoffeeControl.js
@@ -0,0 +1,3 @@
+import React from 'react';
+import NewCoffeeOrder from './NewCoffeeOrder';
+import CoffeeList from './CoffeeList';
---
+++ src/components/CoffeeList.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Coffee from "./Coffee";
+
+function CoffeeList(){
+  return(
+    <>
+      
+    </>
+  )
+}
